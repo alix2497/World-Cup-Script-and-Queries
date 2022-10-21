e69d803a570ebdfcf8c3b847612a64a60b445f47
Al Williams <44587273+alix2497@users.noreply.github.com>
2022-10-21 12:00:36 -0500

    Create queries.sh

---
+++ queries.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo -e "\nTotal number of goals in all games from winning teams:"
+echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
+
+echo -e "\nTotal number of goals in all games from both teams combined:"
+echo "$($PSQL "SELECT SUM(winner_goals + opponent_goals) FROM games")"
+
+echo -e "\nAverage number of goals in all games from the winning teams:"
+echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
+
+echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals), 2) FROM games")"
+
+echo -e "\nAverage number of goals in all games from both teams:"
+echo "$($PSQL "SELECT AVG(winner_goals + opponent_goals) FROM games")"
+
+echo -e "\nMost goals scored in a single game by one team:"
+echo "$($PSQL "SELECT GREATEST( MAX(winner_goals), MAX(opponent_goals) ) FROM games")"
+
+echo -e "\nNumber of games where the winning team scored more than two goals:"
+echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
